32be6e31786924d2879ad978a76721f13eeb0196
irfanturkoz <turkozirfan43@gmail.com>
2025-03-13 06:48:51 +0300

    Superadmin script güncellendi - tüm kullanıcılar temizlendi

--- createSuperAdmin.js
+++ createSuperAdmin.js
@@ -9,27 +9,19 @@ async function createSuperAdmin() {
     await mongoose.connect(mongoUri);
     console.log('MongoDB bağlantısı başarılı');
 
-    // Superadmin kullanıcısını kontrol et
-    let superAdmin = await User.findOne({ username: 'superadmin' });
-    
-    if (superAdmin) {
-      // Varolan superadmin kullanıcısını güncelle
-      superAdmin.password = 'superadmin123';
-      superAdmin.isAdmin = true;
-      superAdmin.isSuperAdmin = true;
-      await superAdmin.save();
-      console.log('Superadmin kullanıcısı güncellendi');
-    } else {
-      // Yeni superadmin kullanıcısı oluştur
-      superAdmin = new User({
-        username: 'superadmin',
-        password: 'superadmin123',
-        isAdmin: true,
-        isSuperAdmin: true
-      });
-      await superAdmin.save();
-      console.log('Yeni superadmin kullanıcısı oluşturuldu');
-    }
+    // Önce tüm kullanıcıları temizle
+    await User.deleteMany({});
+    console.log('Tüm kullanıcılar silindi');
+
+    // Yeni superadmin kullanıcısı oluştur
+    const superAdmin = new User({
+      username: 'superadmin',
+      password: 'superadmin123',
+      isAdmin: true,
+      isSuperAdmin: true
+    });
+    await superAdmin.save();
+    console.log('Yeni superadmin kullanıcısı oluşturuldu');
 
     console.log('Superadmin bilgileri:');
     console.log('Kullanıcı adı: superadmin');
@@ -39,7 +31,7 @@ async function createSuperAdmin() {
     const users = await User.find();
     console.log('\nMevcut kullanıcılar:');
     users.forEach(user => {
-      console.log(`- ${user.username} (Admin: ${user.isAdmin}, SuperAdmin: ${user.isSuperAdmin})`);
+      console.log(`- ${user.username} (Admin: ${user.isAdmin}, SuperAdmin: ${user.isSuperAdmin}, Password: ${user.password})`);
     });
 
   } catch (error) {
